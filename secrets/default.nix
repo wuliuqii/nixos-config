10758ecc5cfade1eaf547f3309cf37196a969e17
gallon <h1090703848@gmail.com>
2023-07-05 22:24:17 +0800

    add ssh config

--- secrets/default.nix
+++ secrets/default.nix
@@ -1,9 +1,19 @@
 { user, ... }:
+let
+  home = "/home/${user}";
+in
 {
   sops.secrets.id_ed22519 = {
     format = "binary";
     sopsFile = ./id_ed25519;
     owner = "${user}";
-    path = "/home/${user}/.ssh/id_ed25519";
+    path = "${home}/.ssh/id_ed25519";
+  };
+
+  sops.secrets.ssh-config = {
+    format = "binary";
+    sopsFile = ./ssh_config;
+    owner = "${user}";
+    path = "${home}/.ssh/config";
   };
 }

b84343ac9d17eb712adf80784204d4e1a17e8178
gallon <h1090703848@gmail.com>
2023-08-21 16:51:02 +0800

    hook codiume lsp

--- home/editor/nvim/default.nix
+++ home/editor/nvim/default.nix
@@ -1,8 +1,40 @@
 { pkgs, ... }:
+let
+  codeium-ls = pkgs.stdenv.mkDerivation
+    {
+      name = "codeium";
+      src = pkgs.fetchurl {
+        url =
+          "https://github.com/Exafunction/codeium/releases/download/language-server-v1.2.66/language_server_linux_x64";
+        sha256 = "QYQLPHFMnX5JH2o75VCxSebWml7OHM5dRsXkvT86jpE=";
+      };
+      nativeBuildInputs = [ pkgs.autoPatchelfHook ];
+      unpackPhase = "true";
+      installPhase = ''
+        ls -lR $src
+        mkdir -p $out/bin
+        cp $src $out/bin/language_server_linux_x64
+        chmod +x $out/bin/language_server_linux_x64
+      '';
+    };
+in
 {
   programs.neovim = {
     enable = true;
     package = pkgs.neovim-nightly;
+  };
+
+  home.packages = with pkgs; [
+    deno
+  ];
+
+  home = {
+    file = {
+      codeium-ls = {
+        target = ".local/share/.codeium/bin/1d8674ba02bba15c9ec356998ccb83b2eaae67d3/language_server_linux_x64";
+        source = "${codeium-ls}/bin/language_server_linux_x64";
+      };
+    };
   };
 
   # home.file.".config/nvim".source = ./config;

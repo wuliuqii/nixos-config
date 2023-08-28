40954c42ca5a492ad7201b443af74c8e8315e408
gallon <h1090703848@gmail.com>
2023-08-28 10:33:10 +0800

    update git config

--- home/shell/git/default.nix
+++ home/shell/git/default.nix
@@ -4,6 +4,15 @@
     git = {
       inherit (config.machine) userName userEmail;
       enable = true;
+      lfs.enable = true;
+      extraConfig = {
+        pull = {
+          rebase = false;
+        };
+        feature = {
+          manyFiles = true;
+        };
+      };
     };
   };
 }

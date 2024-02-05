a754bde61737b948cccc0e995034870dd289779b
gallon <h1090703848@gmail.com>
2024-02-05 17:21:38 +0800

    update nix config

--- flake.nix
+++ flake.nix
@@ -12,6 +12,7 @@
       "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
+    allow-import-from-derivation = true;
   };
 
   inputs = {
